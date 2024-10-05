300a5aea2e37785f3584f04034b67bc216b52761
Rob Thorne <rob@torenware.com>
2024-10-05 14:41:06 -0700

    Stub form

--- beacon/src/App.tsx
+++ beacon/src/App.tsx
@@ -1,10 +1,29 @@
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+  const [sosName, setSOSName] = useState("");
 
   return (
     <>
-      <h1>Hello SOS</h1>
+      <div className="container">
+        <div className="col">
+          <div className="row">
+            <div className="card" >
+              <div className="card-body">
+                <h5 className="card-title">Ask For Help</h5>
+                <form>
+                  <div className="form-item">
+                    <label className="form-label">Your Name</label>
+                    <input className="form-text" type="text" value={sosName} />
+                  </div>
+                </form>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
