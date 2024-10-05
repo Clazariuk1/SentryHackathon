48d927bd98369ae7f392be9d99b300cbe82e61e1
David Sawires <davidashraf@gmail.com>
2024-10-05 16:25:51 -0700

    logging added

--- sos-backend/flask-backend.py
+++ sos-backend/flask-backend.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import requests
 from flask_cors import CORS, cross_origin
 import smtplib
+import logging
 
 CARRIERS = {
     "att": "@mms.att.net",
@@ -13,7 +14,10 @@ CARRIERS = {
 }
 
 EMAIL = "thisisanemergencysos@gmail.com"
-PASSWORD = "PLACE HOLDER PASSWORD"
+PASSWORD = "PASSWORD"
+
+logging.basicConfig(filename='text_messages.log', level=logging.INFO, 
+                    format='%(asctime)s %(levelname)s:%(message)s')
 
 def send_message(recipient, message):
     auth = (EMAIL, PASSWORD)
@@ -22,6 +26,7 @@ def send_message(recipient, message):
         server.starttls()
         server.login(auth[0], auth[1])
         server.sendmail(auth[0], recipient, message)
+        logging.info(f"Message sent to {recipient}")
 
 app = Flask(__name__)
 CORS(app)
@@ -33,14 +38,14 @@ def send_text():
     phone_number = data["phone_number"]
     if not phone_number.startswith("+1"):
         phone_number = "+1" + phone_number
-    print(data)
+    logging.info(f"Received request to send text to {phone_number}")
 
     try:
         response1 = requests.get(
-            f"https://www.ipqualityscore.com/api/json/phone/IPQUALITY_API_KEY/{phone_number}"
+            f"https://www.ipqualityscore.com/api/json/phone/API_KEY/{phone_number}"
         )
         response2 = requests.get(
-            f"https://maps.googleapis.com/maps/api/geocode/json?latlng={data['lat']},{data['lng']}&key=GOOGLE_API_KEY"
+            f"https://maps.googleapis.com/maps/api/geocode/json?latlng={data['lat']},{data['lng']}&key=API_KEY"
         )
         response1_data = response1.json()
         carrier = response1_data["carrier"]
@@ -49,6 +54,12 @@ def send_text():
         message = f"Hey, {data['recipient_name']}! {data['sender_name']} is in danger at {address}. Please help them!"
         recipient = data["phone_number"] + CARRIERS[carrier]
         send_message(recipient, message)
+        logging.info(f"Text sent to {recipient} with message: {message}")
         return jsonify({"status": "success"}), 200
     except Exception as e:
+        logging.error(f"Error sending text: {str(e)}")
         return jsonify({"status": "error", "message": str(e)}), 500
+
+
+if __name__ == "__main__":
+    app.run()
