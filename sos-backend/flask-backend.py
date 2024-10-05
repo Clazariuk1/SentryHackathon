f1362f2d567529bdfb05bfd0793b7c5fd18575fc
David Sawires <davidashraf@gmail.com>
2024-10-05 15:50:15 -0700

    backend flask script

---
+++ sos-backend/flask-backend.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify
+import requests
+from flask_cors import CORS, cross_origin
+import smtplib
+
+CARRIERS = {
+    "att": "@mms.att.net",
+    "tmobile": "@tmomail.net",
+    "Verizon Wireless": "@vtext.com",
+    "sprint": "@messaging.sprintpcs.com",
+    "visible": "@vzwpix.com",
+    "AERIAL COMMUNICATIONS": "@voicestream.net"
+}
+
+EMAIL = "thisisanemergencysos@gmail.com"
+PASSWORD = "PLACE HOLDER PASSWORD"
+
+def send_message(recipient, message):
+    auth = (EMAIL, PASSWORD)
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(auth[0], auth[1])
+        server.sendmail(auth[0], recipient, message)
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/send_text', methods=['POST', 'OPTIONS'])
+@cross_origin()
+def send_text():
+    data = request.json
+    phone_number = data["phone_number"]
+    if not phone_number.startswith("+1"):
+        phone_number = "+1" + phone_number
+    print(data)
+
+    try:
+        response1 = requests.get(
+            f"https://www.ipqualityscore.com/api/json/phone/IPQUALITY_API_KEY/{phone_number}"
+        )
+        response2 = requests.get(
+            f"https://maps.googleapis.com/maps/api/geocode/json?latlng={data['lat']},{data['lng']}&key=GOOGLE_API_KEY"
+        )
+        response1_data = response1.json()
+        carrier = response1_data["carrier"]
+
+        address = response2.json()["results"][0]["formatted_address"]
+        message = f"Hey, {data['recipient_name']}! {data['sender_name']} is in danger at {address}. Please help them!"
+        recipient = data["phone_number"] + CARRIERS[carrier]
+        send_message(recipient, message)
+        return jsonify({"status": "success"}), 200
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
