55f69941fd65bdc8e948a7a10bdd1da4acadeec2
David Sawires <davidashraf@gmail.com>
2024-10-05 19:09:58 -0700

    just sends an email now

--- sos-backend/flask-backend.py
+++ sos-backend/flask-backend.py
@@ -4,19 +4,10 @@ from flask_cors import CORS, cross_origin
 import smtplib
 import logging
 
-CARRIERS = {
-    "att": "@mms.att.net",
-    "tmobile": "@tmomail.net",
-    "Verizon Wireless": "@vtext.com",
-    "sprint": "@messaging.sprintpcs.com",
-    "visible": "@vzwpix.com",
-    "AERIAL COMMUNICATIONS": "@voicestream.net"
-}
-
 EMAIL = "thisisanemergencysos@gmail.com"
 PASSWORD = "PASSWORD"
 
-logging.basicConfig(filename='text_messages.log', level=logging.INFO, 
+logging.basicConfig(filename='text_messages.log', level=logging.INFO,
                     format='%(asctime)s %(levelname)s:%(message)s')
 
 def send_message(recipient, message):
@@ -25,7 +16,7 @@ def send_message(recipient, message):
     with smtplib.SMTP("smtp.gmail.com", 587) as server:
         server.starttls()
         server.login(auth[0], auth[1])
-        server.sendmail(auth[0], recipient, message)
+        server.sendmail(auth[0], recipient, f"Subject: THIS IS AN EMERGENCY!!!\n\n{message}")
         logging.info(f"Message sent to {recipient}")
 
 app = Flask(__name__)
@@ -35,31 +26,19 @@ CORS(app)
 @cross_origin()
 def send_text():
     data = request.json
-    phone_number = data["phone_number"]
-    if not phone_number.startswith("+1"):
-        phone_number = "+1" + phone_number
-    logging.info(f"Received request to send text to {phone_number}")
+    logging.info(data)
+    recipient = data["emergencyPhone"]
 
     try:
-        response1 = requests.get(
-            f"https://www.ipqualityscore.com/api/json/phone/API_KEY/{phone_number}"
-        )
         response2 = requests.get(
-            f"https://maps.googleapis.com/maps/api/geocode/json?latlng={data['lat']},{data['lng']}&key=API_KEY"
+            f"https://maps.googleapis.com/maps/api/geocode/json?latlng={data['latitude']},{data['longitude']}&key=API_KEY"
         )
-        response1_data = response1.json()
-        carrier = response1_data["carrier"]
 
         address = response2.json()["results"][0]["formatted_address"]
-        message = f"Hey, {data['recipient_name']}! {data['sender_name']} is in danger at {address}. Please help them!"
-        recipient = data["phone_number"] + CARRIERS[carrier]
+        message = f"Hello! {data['fullName']} is in danger at {address} {data['location']}. Please help them!"
         send_message(recipient, message)
         logging.info(f"Text sent to {recipient} with message: {message}")
-        return jsonify({"status": "success"}), 200
+        return jsonify({"status": f"{message} sent to {recipient}"}), 200
     except Exception as e:
         logging.error(f"Error sending text: {str(e)}")
         return jsonify({"status": "error", "message": str(e)}), 500
-
-
-if __name__ == "__main__":
-    app.run()
